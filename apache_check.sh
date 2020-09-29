285b25ed08d5c0f8c6a69edb9304d95a917e60c9
deemoprobe <deemoprobe@gmail.com>
2020-09-29 11:17:19 +0800

    apache_check

--- apache_check.sh
+++ apache_check.sh
@@ -2,5 +2,13 @@
 # @Author: deemoprobe@gmail.com
 # @Date:   2020-09-28 09:08:12
 # @Last Modified by:   deemoprobe@gmail.com
-# @Last Modified time: 2020-09-28 09:08:17
+# @Last Modified time: 2020-09-29 11:17:17
 
+##### 排查查找路径 #####
+u="/tmp"
+
+##### Apache安全规范检查主体 #####
+APACHEFUNC()
+{
+    
+}

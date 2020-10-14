44b4776534c58e50cee2a4f40fbdfd1417925f60
deemoprobe <deemoprobe@gmail.com>
2020-10-14 16:26:58 +0800

    add some to apache_check

--- apache_check.sh
+++ apache_check.sh
@@ -2,7 +2,7 @@
 # @Author: deemoprobe@gmail.com
 # @Date:   2020-09-28 09:08:12
 # @Last Modified by:   deemoprobe@gmail.com
-# @Last Modified time: 2020-09-29 11:17:17
+# @Last Modified time: 2020-10-14 16:26:34
 
 ##### 排查查找路径 #####
 u="/tmp"
@@ -10,5 +10,12 @@ u="/tmp"
 ##### Apache安全规范检查主体 #####
 APACHEFUNC()
 {
-    
+    APACHE_DIR=`find / \( -path $u \) -prune -o -name 'httpd.conf' 2>/dev/null | grep -v original | grep -v docs | grep -v tmp`
+    if [ $? = 0 ]
+    then
+        for APACHE_PATH in $APACHE_DIR
+        do
+            APACHEPATH=`ls $APACHE_PATH | awk -F "/conf" '{print $1}'`
+            CONFIGPATH=`$APACHEPATH/conf`
+            
 }

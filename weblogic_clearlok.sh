4d5b4130ab951aaf65276d5c47772b85e524ff19
deemoprobe <deemoprobe@gmail.com>
2021-03-08 15:25:22 +0800

    weblogic clearlok

---
+++ weblogic_clearlok.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @Author: deemoprobe@gmail.com
+# @Date:   2021-03-08 15:20:37
+# @Last Modified by:   deemoprobe@gmail.com
+# @Last Modified time: 2021-03-08 15:21:43
+
+find /app/weblogic/user_projects/domains/testdomain -name "*.lok" | xargs rm -rf

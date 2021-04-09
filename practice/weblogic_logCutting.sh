2de7571b669709fcb4e92ab05f4a1f50bbc5f611
deemoprobe <deemoprobe@gmail.com>
2021-04-09 11:05:14 +0800

    change weblogic_logCutting

--- practice/weblogic_logCutting.sh
+++ practice/weblogic_logCutting.sh
@@ -2,7 +2,7 @@
 # @Author: deemoprobe@gmail.com
 # @Date:   2021-03-08 14:39:29
 # @Last Modified by:   deemoprobe@gmail.com
-# @Last Modified time: 2021-03-09 15:27:15
+# @Last Modified time: 2021-04-09 11:04:11
 # @Description: Weblogic LogCutting Script and Clear Rules
 ####################### 后续操作 ########################
 # 由于echo > appserver*.out时appserver*.out一直在刷新日志
@@ -13,64 +13,25 @@
 # sed -i s/">"/">>"/g startappserver*.sh, 重启受管服务即可
 #########################################################
 
-export DEPLOY_DIR=/tpsys/applications/proddomain/logs
+export LOGS_DIR=/app/applications/proddomain/logs
 
-ps -ef | grep java | grep weblogic | grep appserver1
-if [ $? = 0 ]
-then
-        cp ${DEPLOY_DIR}/appserver1.out ${DEPLOY_DIR}/appserver1-`date +%Y-%m-%d-%H:%M:%S`.out
-        echo  > ${DEPLOY_DIR}/appserver1.out
-        cp ${DEPLOY_DIR}/appserver1.err ${DEPLOY_DIR}/appserver1-`date +%Y-%m-%d-%H:%M:%S`.err
-        echo  > ${DEPLOY_DIR}/appserver1.err
-fi
+# 遍历6个appserver
+for i in {1..6}
+do
+    ps -ef | grep java | grep weblogic | grep appserver$i &> /dev/null
 
-ps -ef | grep java | grep weblogic | grep appserver2
-if [ $? = 0 ]
-then
-        cp ${DEPLOY_DIR}/appserver2.out ${DEPLOY_DIR}/appserver2-`date +%Y-%m-%d-%H:%M:%S`.out
-        echo  > ${DEPLOY_DIR}/appserver2.out
-        cp ${DEPLOY_DIR}/appserver2.err ${DEPLOY_DIR}/appserver2-`date +%Y-%m-%d-%H:%M:%S`.err
-        echo  > ${DEPLOY_DIR}/appserver2.err
-fi
-
-ps -ef | grep java | grep weblogic | grep appserver3
-if [ $? = 0 ]
-then
-        cp ${DEPLOY_DIR}/appserver3.out ${DEPLOY_DIR}/appserver3-`date +%Y-%m-%d-%H:%M:%S`.out
-        echo  > ${DEPLOY_DIR}/appserver3.out
-        cp ${DEPLOY_DIR}/appserver3.err ${DEPLOY_DIR}/appserver3-`date +%Y-%m-%d-%H:%M:%S`.err
-        echo  > ${DEPLOY_DIR}/appserver3.err
-fi
-
-ps -ef | grep java | grep weblogic | grep appserver4
-if [ $? = 0 ]
-then
-        cp ${DEPLOY_DIR}/appserver4.out ${DEPLOY_DIR}/appserver4-`date +%Y-%m-%d-%H:%M:%S`.out
-        echo  > ${DEPLOY_DIR}/appserver4.out
-        cp ${DEPLOY_DIR}/appserver4.err ${DEPLOY_DIR}/appserver4-`date +%Y-%m-%d-%H:%M:%S`.err
-        echo  > ${DEPLOY_DIR}/appserver4.err
-fi
-
-ps -ef | grep java | grep weblogic | grep appserver5
-if [ $? = 0 ]
-then
-        cp ${DEPLOY_DIR}/appserver5.out ${DEPLOY_DIR}/appserver5-`date +%Y-%m-%d-%H:%M:%S`.out
-        echo  > ${DEPLOY_DIR}/appserver5.out
-        cp ${DEPLOY_DIR}/appserver5.err ${DEPLOY_DIR}/appserver5-`date +%Y-%m-%d-%H:%M:%S`.err
-        echo  > ${DEPLOY_DIR}/appserver5.err
-fi
-
-ps -ef | grep java | grep weblogic | grep appserver6
-if [ $? = 0 ]
-then
-        cp ${DEPLOY_DIR}/appserver6.out ${DEPLOY_DIR}/appserver6-`date +%Y-%m-%d-%H:%M:%S`.out
-        echo  > ${DEPLOY_DIR}/appserver6.out
-        cp ${DEPLOY_DIR}/appserver6.err ${DEPLOY_DIR}/appserver6-`date +%Y-%m-%d-%H:%M:%S`.err
-        echo  > ${DEPLOY_DIR}/appserver6.err
-fi
+    if [ $? = 0 ]
+    then
+        cp ${LOGS_DIR}/appserver$i.out ${LOGS_DIR}/appserver$i-`date +%Y-%m-%d-%H:%M:%S`.out
+        echo  > ${LOGS_DIR}/appserver$i.out
+        cp ${LOGS_DIR}/appserver$i.err ${LOGS_DIR}/appserver$i-`date +%Y-%m-%d-%H:%M:%S`.err
+        echo  > ${LOGS_DIR}/appserver$i.err
+    fi
+    
+done
 
 # Clear Logs
-find ${DEPLOY_DIR}/ -name "appserver*" -mtime +30 -exec rm -rf {} \;
+find ${LOGS_DIR}/ -name "appserver*" -mtime +30 -exec rm -rf {} \;
 
 # gzip
-#find ${DEPLOY_DIR}/ -name "appserver*" ! -name "*.gz" -mtime +5 -exec gzip {} \;
+#find ${LOGS_DIR}/ -name "appserver*" ! -name "*.gz" -mtime +5 -exec gzip {} \;
---
+++ test.sh
@@ -0,0 +1,10 @@
+# @Author: deemoprobe@gmail.com
+# @Date:   2021-04-09 10:53:55
+# @Last Modified by:   deemoprobe@gmail.com
+# @Last Modified time: 2021-04-09 11:04:42
+#!/bin/bash
+
+for i in {1..6}
+do
+    echo appserver$i
+done
